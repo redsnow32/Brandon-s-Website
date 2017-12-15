e112f09da9eecaf566b220fc8f0053c4d0529172
Brandon Allred <b32alls@gmail.com>
2017-12-14 18:03:29 -0700

    webinar javascript practice for DevMTn

---
+++ script.js
@@ -0,0 +1,18 @@
+var cuteFuzzyDuck = {
+  species: "Duck",
+  qualities: ["good swimmers", "cute", "fuzzy", "quackiness"],
+  mate: {
+    name: "Wallace",
+    species: "Duck",
+    qualities: ["so-so swimmer", "ugly", "not fuzzy", "quacky"],
+  }
+  cuteFuzzyDuck["name"]
+  cuteFuzzyDuck.qualities[1]
+  cuteFuzzyDuck["mate"].qualities[0]
+},
+quack: function() {
+  return "quack!"
+  }
+}
+
+cuteFuzzyDuck.quack()
